35272716bcdb626a67960e720d1b11e04f6dd9c7
Deborah-Araujo <deby.limara@gmail.com>
2025-06-24 17:27:51 -0300

    update: MVC organization

---
+++ src/controllers/homepageController.js
@@ -0,0 +1,10 @@
+
+function homepageView (req, res) {
+    res.render('homepage.html')
+    // Posso mudar para o seguinte código, caso necessário:
+    // res.render('homepage');
+}
+
+module.exports = {
+    homepageView
+}
