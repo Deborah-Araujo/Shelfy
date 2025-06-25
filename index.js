20bc48a017a5e41f14ebf061332f9b1f51affe46
Emanuelly <emanuellygomes.manu2004@gmail.com>
2025-06-25 01:41:25 -0300

    feat: realiza a configuração com o banco, cria model do usuário e efetiva login e cadastro

--- index.js
+++ index.js
@@ -1,19 +1,30 @@
 const express = require('express')
 const mustacheExpress = require('mustache-express')
+const session = require('express-session');
 
+const db  = require('./db');
 const app = express()
 
 app.engine('html', mustacheExpress());
 app.set('view engine', 'html');
 
 app.set('views', __dirname + '/src/views');
-// Tentando usar imagens no homepage.html
+
 app.use('/static', express.static(__dirname + '/static'));
 
 app.use(express.urlencoded({extended: true}));
 
+app.use(session({
+    secret: 'secret-token',
+    name: 'sessionId',
+    resave: false,
+    saveUninitialized: false
+}));
+
 app.use('/', require('./src/routes/homepageRoutes')) 
 app.use('/user', require('./src/routes/usuarioRoutes')) //Fica sendo um prefixo pra rotas do usuário. Pra acessar login, por exemplo, tem que acessar: /user/login
+
+db.sync();
 
 const PORT = 8080;
 app.listen(PORT, ()=>{
--- src/routes/usuarioRoutes.js
+++ src/routes/usuarioRoutes.js
@@ -6,4 +6,7 @@ const usuarioController = require('../controllers/usuarioController')
 router.get('/login', usuarioController.loginView)
 router.get('/cadastro', usuarioController.cadastroView)
 
+router.post('/cadastrar_usuario', usuarioController.postCadastrarUsuario);
+router.post('/autenticar', usuarioController.postAutenticarUsuario);
+
 module.exports = router
